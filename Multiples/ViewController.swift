aa17de0cdd4aaa250d912f23883f3879fb5666b3
Chad Wollenberg <chad.wollenberg@me.com>
2016-01-19 20:43:35 -0600

    first push of first app I've tried to tackle from start to finish. an assignment for a stacksocial class

--- Multiples/ViewController.swift
+++ Multiples/ViewController.swift
@@ -9,17 +9,80 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //properties
+    var Multnum:Int = 0
+    var MaxNum:Int = 0
+    var currentNum:Int = 0
+    var workingNum:Int = 0
+    
+    
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    //outlets
+    @IBOutlet weak var titleLogo: UIImageView!
+    @IBOutlet weak var playBtn: UIButton!
+    @IBOutlet weak var multipleField: UITextField!
+    
+    @IBOutlet weak var addBtn: UIButton!
+    @IBOutlet weak var statLbl: UILabel!
+
+    
+    @IBAction func onPlayBtnPressed (sender: UIButton!) {
+        
+        if multipleField.text != nil && multipleField.text != "" {
+            playBtn.hidden = true
+            multipleField.hidden = true
+            
+            statLbl.hidden = false
+            addBtn.hidden = false
+            
+            Multnum = Int(multipleField.text!)!
+            currentNum = Multnum
+            statLbl.text = "tap 'add' to add!"
+            findMaxNum()
+        }
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func onAddBtnPressed (sender: UIButton!) {
+        addcurrentandmultiple()
+        updatestatLbl()
+        if isGameOver(){
+            restartGame()
+        }
     }
-
+    //functions
+    
+    func addcurrentandmultiple() {
+        workingNum = currentNum
+       currentNum = Multnum + currentNum
+    }
+    
+    func updatestatLbl() {
+        statLbl.text = " \(workingNum) + \(Multnum) = \(currentNum) "
+    }
+    func findMaxNum(){
+        MaxNum = Multnum * 20
+    }
+    func isGameOver() ->Bool {
+        if currentNum >= MaxNum {
+            return true
+        } else {
+            return false
+        }
+        
+    }
+    func restartGame() {
+        playBtn.hidden = false
+        multipleField.hidden = false
+        
+        statLbl.hidden = true
+        addBtn.hidden = true
+        currentNum = 0
+        Multnum = 0
+        MaxNum = 0
+        workingNum = 0
+    }
 
 }
-
